64d3802e2e80778caacbbd01729f9094014b2e0a
Adrien LAC <alac@e2r11p23.42.fr>
2018-12-07 14:12:37 +0100

    ajout

--- ft_p_address.c
+++ ft_p_address.c
@@ -6,7 +6,7 @@
 /*   By: alac <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 11:04:02 by alac              #+#    #+#             */
-/*   Updated: 2018/12/07 13:21:39 by lmariott         ###   ########.fr       */
+/*   Updated: 2018/12/07 14:11:32 by alac             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ int		ft_p_address(va_list *ap, char *tab)
 		return (0);
 	x = va_arg(*ap, void *);
 	ft_putstr("0x");
-	ft_putll_base((long long)x, 16);
+	ft_putull_base_min((unsigned long long)x, 16);
 	return (0);
 }
